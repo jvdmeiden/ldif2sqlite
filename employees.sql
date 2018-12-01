c6ef271b29bf4f2474dcd7f22e2e0bd2ca369761
jvdmeiden@googlemail.com <jvdmeiden@googlemail.com>
2018-12-01 17:52:07 +0100

    added sql to create table

---
+++ employees.sql
@@ -0,0 +1,17 @@
+CREATE TABLE employees (
+ ldifDate text NOT NULL,
+ displayName text NOT NULL,
+ uid text,
+ telephoneNumber text,
+ info text,
+ department text,
+ company text,
+ employeeType text,
+ name text,
+ manager text,
+ country text,
+ city text,
+ mail text,
+ mobile text,
+ photo text
+) ;
