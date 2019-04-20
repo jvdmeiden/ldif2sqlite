5de0ba205c81180a45e0ce2cf7f81b955b0c00ad
Jan van der Meiden <jvdmeiden@googlemail.com>
2019-04-20 18:28:38 +0200

    Update ldif_pics.py

--- ldif_pics.py
+++ ldif_pics.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-#
 # Copyright (c) 2018 Jan van der Meiden.                            
 # Copying and distribution of this file, with or without modification, 
 # are permitted in any medium without royalty provided the copyright   
@@ -8,21 +5,25 @@
 # without any warranty.   
 #
 # This is a python script that extracts the pictured from an ldif file 
-# it requires 1 parameters: the ldif file name
-# If you expect duplicates (like when running on ldifs from different i
-# dates) dedplicate using soemething like "rdfind -makehardlinks true ."
-# Works with https://pypi.org/project/ldif3/3.2.2/#files
-# Do not yet really understand what I am doing here...
+# it requires 2 parameters: the ldif file name and the date from the ldif file
+# (too lazy to extract this from something else)
+# It writes the images to a folder img
+#
+# Requires ldif3: 
+# > pip install ldif3
 import sys
 import collections
 from ldif3 import LDIFParser
 
 parser = LDIFParser(open(sys.argv[1], 'rb'))
+date = sys.argv[2]
 for dn, entry in parser.parse():
     for k, v in entry.items():
-        if k == 'jpegPhoto' or k =='thumbnailPhoto': 
+        if k == 'jpegPhoto' or k =='thumbnailPhoto':
+            print(entry['userPrincipalName'][0])
             print("img/" + entry['c'][0]+"."+entry['userPrincipalName'][0]+"."+date+".jpg")
-            file = open("img/" + entry['c'][0]+"."+entry['userPrincipalName'][0]+"."+date+".jpg",'wb') 
+            file = open("img/" + entry['c'][0]+"."+entry['userPrincipalName'][0]+"."+date+".jpg",'wb')
             if (isinstance(v[0], (bytes, bytearray))):
-              file.write(v[0]) 
+              file.write(v[0])
             file.close()
+
